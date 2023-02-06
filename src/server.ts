cef3eeb6a289a9e1a331e53c8ccb581ec82e96a3
reinaldo-alves <123407449+reinaldo-alves@users.noreply.github.com>
2023-02-06 13:10:29 -0300

    Add main files

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { userRoutes } from './routes/user.routes';
+import { videosRoutes } from './routes/videos.routes';
+import { config } from 'dotenv';
+
+config();
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+app.use('/user', userRoutes);
+app.use('/videos', videosRoutes);
+
+app.listen(port)
